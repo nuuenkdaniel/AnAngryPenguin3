95c82d1522857dc0b8bf795a540839901737c1e0
nuuenkdaniel <lewdaniel2@gmail.com>
2023-08-01 00:16:54 -0400

    letters coords fixed

--- commands/mvPiece.js
+++ commands/mvPiece.js
@@ -19,23 +19,27 @@ module.exports = {
             message.reply("It is not your turn");
             return;
         }
-        if(args > 2) {
+        if(args.length > 2) {
             message.reply("Invalid input try again");
             return;
         }
         const moves = args.join('').split('');
-        for(let i = 0, j = 0; i < 8 && j <= 2; i++) {
+        for(let i = 0, j = 0; i < 8 && j < 4; i++) {
             const letters = ["a","b","c","d","e","f","g","h"];
+            console.log(i);
             if(moves[j] === letters[i]) {
                 moves[j] = i;
                 i = -1;
-                j += 2;
+                j++;
             }
+            else if(i === 7) {
+                i = -1;
+                j++;
+            } 
         }
         for(let i = 0; i < moves.length; i++) moves[i] = Number(moves[i]);
         moves[1] = 8-moves[1];
         moves[3] = 8-moves[3];
-        console.log(moves);
         const chessBoard = new ChessBoard(8,8,25);
         await chessBoard.dbBoard(message.guildId);
         const piece = chessBoard.getTile(moves[0],moves[1]).getPiece();
